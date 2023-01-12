3080c2eb06a1af94dabf10d01e4d352bc27bc29c
User <User@DESKTOP-EQNJDV3>
2023-01-12 11:16:53 -0600

    Add project files.

---
+++ github_practice.cpp
@@ -0,0 +1,13 @@
+// Name: Conner Swoboda
+// Date: 1/12/2023
+// Program Description: Practice program for syncing with GitHub
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello World!";
+
+}
